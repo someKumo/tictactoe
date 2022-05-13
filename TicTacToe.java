90b54447c3ec035b091ec8062e404d6db658d2f8
someKumo <73191767+someKumo@users.noreply.github.com>
2022-05-13 15:08:35 +0200

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -5,7 +5,17 @@ import java.util.*;
 public class TicTacToe {
 
 	static String[] board;
-	static String turn;
+	static String werIstAmZug;
+
+	public static void ausgabeSpielBrett() {
+		System.out.println();
+		System.out.println(board[6] + "|" + board[7] + "|" + board[8]);
+		System.out.println("-+-+-");
+		System.out.println(board[3] + "|" + board[4] + "|" + board[5]);
+		System.out.println("-+-+-");
+		System.out.println(board[0] + "|" + board[1] + "|" + board[2]);
+		System.out.println();
+	}
 
 	static String winnerCheck() {
 		for (int a = 0; a < 8; a++) {
@@ -38,10 +48,12 @@ public class TicTacToe {
 				break;
 			}
 			if (line.equals("XXX")) {
-				return "X";}
+				return "X";
+			}
 
 			else if (line.equals("OOO")) {
-				return "O";}
+				return "O";
+			}
 		}
 
 		for (int a = 0; a < 9; a++) {
@@ -51,24 +63,14 @@ public class TicTacToe {
 				return "unentschieden";
 			}
 		}
-		System.out.println(turn + " ist an der Reihe!");
+		System.out.println(werIstAmZug + " ist an der Reihe!");
 		return null;
-	}
-
-	public static void ausgabeSpielBrett() {
-		System.out.println();
-		System.out.println(board[6] + "|" + board[7] + "|" + board[8]);
-		System.out.println("-+-+-");
-		System.out.println(board[3] + "|" + board[4] + "|" + board[5]);
-		System.out.println("-+-+-");
-		System.out.println(board[0] + "|" + board[1] + "|" + board[2]);
-		System.out.println();
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		board = new String[9];
-		turn = "X";
+		werIstAmZug = "X";
 		String winner = null;
 
 		for (int a = 0; a < 9; a++) {
@@ -76,39 +78,68 @@ public class TicTacToe {
 		}
 		ausgabeSpielBrett();
 
-		System.out.println("X f�ngt an!");
-
+		System.out.println("X fängt an!");
+		
+		// gamegoing schleife?
 		while (winner == null) {
 			int numInput;
-			try {
-				numInput = sc.nextInt();
-				if (!(numInput > 0 && numInput <= 9)) {
+			
+			if (werIstAmZug == "O") {
+				
+				
+		        int max = 9;
+		        int min = 1;
+		        int range = max - min + 1;
+		        int random = (int)(Math.random() * range) + min; 
+		        System.out.println("KI wählt: " + random);
+		        numInput = random;
+
+			        if (board[numInput - 1].equals(String.valueOf(numInput))) {
+						board[numInput - 1] = werIstAmZug;
+					
+						if (werIstAmZug.equals("X")) {
+							werIstAmZug = "O";
+						} else {
+							werIstAmZug = "X";
+						}
+						winner = winnerCheck();
+						ausgabeSpielBrett();
+					}
+		        
+			} else {
+				try {
+					numInput = sc.nextInt();
+					if (!(numInput > 0 && numInput <= 9)) {
+						System.out.println("Gib eine Zahl zwischen 1-9 ein!");
+						continue;
+					}
+				} catch (InputMismatchException e) {
 					System.out.println("Gib eine Zahl zwischen 1-9 ein!");
 					continue;
 				}
-			} catch (InputMismatchException e) {
-				System.out.println("Gib eine Zahl zwischen 1-9 ein!");
-				continue;
-			}
-			if (board[numInput - 1].equals(String.valueOf(numInput))) {
-				board[numInput - 1] = turn;
-
-				if (turn.equals("X")) {
-					turn = "O";
+				if (board[numInput - 1].equals(String.valueOf(numInput))) {
+					board[numInput - 1] = werIstAmZug;
+				
+					if (werIstAmZug.equals("X")) {
+						werIstAmZug = "O";
+					} else {
+						werIstAmZug = "X";
+					}
+					winner = winnerCheck();
+					ausgabeSpielBrett();
 				} else {
-					turn = "X";
+					System.out.println("Belegt!");
 				}
-
-				ausgabeSpielBrett();
-				winner = winnerCheck();
-			} else {
-				System.out.println("Belegt!");
 			}
+			
+			
 		}
 		if (winner.equalsIgnoreCase("unentschieden")) {
 			System.out.println("Unentschieden!");
+			sc.close();
 		} else {
 			System.out.println(winner + " gewinnt!");
+			sc.close();
 		}
 	}
 }
